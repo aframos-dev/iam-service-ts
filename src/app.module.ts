5eb2920c9bb15c07718cfe248f99296f4ce1d571
Ramos <andfera201@gmail.com>
2026-06-10 16:12:22 -0500

    add NODE_ENV .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ConfigModule } from '@nestjs/config';
-import { envValidation } from './config/env.validation';
+import { ConfigModule } from '@nestjs/config'
+import { envValidation } from './config/env.validation'
 
 @Module({
   imports: [
