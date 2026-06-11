0998c19a9c5ea7920a5ee9ddad3d7af6ff56d408
Ramos <andfera201@gmail.com>
2026-06-10 21:52:55 -0500

    add prisma orm

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -3,4 +3,5 @@ import * as Joi from 'joi'
 export const envValidation = Joi.object({
   NODE_ENV: Joi.string().valid('dev', 'production').required(),
   PORT: Joi.number().port().required(),
+  DATABASE_URL: Joi.string().uri().required(),
 })
